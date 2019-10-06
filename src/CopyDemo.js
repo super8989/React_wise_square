46ee45ffc181dbf60ba185cb8b86242b3f7a736a
Sam <super8989@gmail.com>
2019-10-06 13:43:32 -0400

    added in onCopy method to CopyDemo component:

--- src/CopyDemo.js
+++ src/CopyDemo.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
 
 class CopyDemo extends Component {
+  handleCopy() {
+    alert("Can't Copy This Na Na Na Na");
+  }
+
   render() {
     return (
       <div>
         <h3>Try Copying Some of this Text: </h3>
-        <section style={{ width: "300px", display: "inline-block " }}>
+        <section
+          style={{ width: "300px", display: "inline-block " }}
+          onCopy={this.handleCopy}
+        >
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non
           quis exercitationem culpa nesciunt nihil aut nostrum explicabo
           reprehenderit optio amet ab temporibus asperiores quasi cupiditate.
